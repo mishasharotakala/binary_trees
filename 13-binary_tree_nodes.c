750037c5f3d227b543769a26c9eb2ef44a09bf3b
mishasharotakala <103355881+mishasharotakala@users.noreply.github.com>
2022-09-21 13:49:22 +0200

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,13 +1,33 @@
 #include "binary_trees.h"
+
 /**
- *binary_tree_nodes - Count the nodes that have at least one child
- *@tree: The tree to be evaluated
- *Return: 0 when tree is NULL or a recursive function call
+ * binary_tree_is_leaf - checks if a node is a leaf
+ * @node: pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, and 0 otherwise. If node is NULL, return 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node != NULL && node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
+}
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the nodes in
+ *
+ * Description: A NULL pointer is not a node
+ *
+ * Return: number of nodes with at least 1 child in a binary tree
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree != NULL && (tree->left != NULL || tree->right != NULL))
-		return (1 + binary_tree_nodes(tree->left) +
-			binary_tree_nodes(tree->right));
-	return (0);
+	size_t left, right;
+
+	if (tree == NULL || binary_tree_is_leaf(tree))
+		return (0);
+	left = binary_tree_nodes(tree->left);
+	right = binary_tree_nodes(tree->right);
+	return (1 + left + right);
 }
